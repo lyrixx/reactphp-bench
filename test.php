2f89466d5a6b92b88d44f44ebd950c48d2fb81a6
Grégoire Pineau <lyrixx@lyrixx.info>
2013-01-12 18:47:53 +0100

    Added noise to generate memory

--- test.php
+++ test.php
@@ -3,9 +3,17 @@
 require 'vendor/autoload.php';
 
 $i = 0;
+
 $app = function ($request, $response) use(&$i) {
     $response->writeHead(200, array('Content-Type' => 'text/plain'));
+
+    $objects = array();
+    for ($j=0; $j < 1000; $j++) {
+        $objects[] = new DateTime();
+    }
+
     $response->end("Hello World\n");
+
     if (0 == $i % 100) {
         // echo sprintf("[%5.0f] Mem(c): %4.3fMb, Mem(p): %4.3fMb\n", $i, memory_get_usage()/(1024*1024), memory_get_peak_usage()/(1024*1024));
         echo sprintf("%s;%4.3f;%4.3f;\n", $i, memory_get_usage()/(1024*1024), memory_get_peak_usage()/(1024*1024));
